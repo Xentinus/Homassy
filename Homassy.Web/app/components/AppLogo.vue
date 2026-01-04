aa13f51bd99e5b2f8fd2070897a93730ea6dafae
Béla Kellner <bela.kellner@icloud.com>
2026-01-04 14:43:57 +0100

    Adjust AppLogo sizing and responsive breakpoints
    Updated the AppLogo component to use smaller image and text sizes and changed the responsive breakpoint from 400px to 375px for improved display on smaller screens.

--- Homassy.Web/app/components/AppLogo.vue
+++ Homassy.Web/app/components/AppLogo.vue
@@ -1,6 +1,6 @@
 ﻿<template>
   <div class="flex items-center gap-2 md:gap-3">
-    <img src="/favicon.svg" alt="Homassy Logo" class="hidden min-[400px]:block w-10 h-10 md:w-12 md:h-12">
-    <span class="font-semibold text-lg md:text-2xl">Homassy</span>
+    <img src="/favicon.svg" alt="Homassy Logo" class="hidden min-[375px]:block w-8 h-8 md:w-10 md:h-10">
+    <span class="font-semibold text-base md:text-lg">Homassy</span>
   </div>
 </template>
